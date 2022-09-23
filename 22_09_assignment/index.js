2a6ecd7ff332e8672a8402e35591d480f9837347
sharath <sharathchander.garepelli@codexbox.com>
2022-09-23 08:49:25 +0530

    add

--- 22_09_assignment/index.js
+++ 22_09_assignment/index.js
@@ -3,13 +3,19 @@
 // 2)each sub pass mark is 35
 // 3)if the student is pass then print totalmarks,grade,percentage
 // 4)if the student is fail then fail will print
-let maths=100;
-let science=45;
-let english=50;
-let total = maths+science+english;
-if(maths>=35 && science>=35 && english>=35){
-    console.log(`student is passed with ${total} marks`)
+let chemistry=100;
+let physics=45;
+let biology=50;
+let total = chemistry+physics+biology;
+percentage=total/3*100
+if(chemistry>=35 && physics>=35 && biology>=35){
+    console.log(`student is passed with marks: ${total}`);
+    console.log(`percentage: ${percentage}%`);
 }
 else{
     console.log('fail')
 }
+if(total>=250){
+    console.log(`grade: A`)
+}
+else if(total<249 || total)
